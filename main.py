2bb23a50c1f8b1446a938eaaf93a1a577980c6fc
anthonysiu2000 <tonysiu123@yahoo.com>
2022-02-13 14:36:10 -0500

    added cascade xml file

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
 import cv2 as cv
 
+# Read image from your local file system
+original_image = cv.imread('C:/Users/tonys/Downloads/jonSample.jpg')
+
+# Convert color image to grayscale for Viola-Jones
+grayscale_image = cv.cvtColor(original_image, cv.COLOR_BGR2GRAY)
+
+# Load the classifier and create a cascade object for face detection
+face_cascade = cv.CascadeClassifier('./haarcascade_frontalface_alt.xml')
+
+detected_faces = face_cascade.detectMultiScale(grayscale_image)
+
+for (column, row, width, height) in detected_faces:
+    cv.rectangle(
+        original_image,
+        (column, row),
+        (column + width, row + height),
+        (0, 255, 0),
+        2
+    )
+
+cv.imshow('Image', original_image)
+cv.waitKey(0)
+cv.destroyAllWindows()
