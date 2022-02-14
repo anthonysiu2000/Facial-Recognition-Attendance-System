8b627c5b032c597f07e5e88d7b320eb85d750e71
anthonysiu2000 <tonysiu123@yahoo.com>
2022-02-14 18:37:22 -0500

    changed to single photo database

--- main.py
+++ main.py
@@ -5,77 +5,52 @@ import os
 
 #obtains the folder/database with attendance images
 current_dir = os.path.dirname(__file__)
-path = os.path.join(current_dir, "./baseImages")
+path = os.path.join(current_dir, "./testImages/obama.jpg")
 
-#sets up storage for images and students, respectively
-images = []
-studentNames = []
+#load image
+img = cv.imread(path)
+studentName = "Obama"
 
-#iterates through the "baseImages" folder, appending each file to an array for storage
-def loadImages():
-    myList = os.listdir(path)
-    for img in myList:
-        curImg = cv.imread(f'{path}/{img}')
-        images.append(curImg)
-        studentNames.append(os.path.splitext(img)[0])
-
-#function to iterate through the image list and get their 128 long encodings
-def findEncodings(images):
-    encodings = []
-    for img in images:
-        img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-        encode = fr.face_encodings(img)[0]
-        encodings.append(encode)
-    return encodings
-
-
-
-#primary function calls
-loadImages()
-encodeList = findEncodings(images)
-
-
+#get the 128 long encodings
+img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+encode = fr.face_encodings(img)[0]
+encodings = []
+encodings.append(encode)
 
 #start a live video capture
 cap = cv.VideoCapture(0)
 
 while True:
     #obtain current frame
-    success, img = cap.read()
+    success, imgL = cap.read()
 
     #larger resolution for screen
-    img = cv.resize(img, (0,0), None, 2, 2)
+    imgL = cv.resize(imgL, (0,0), None, 2, 2)
 
     #decrease frame size in order to help with computation
-    imgS = cv.resize(img, (0,0), None, 0.125, 0.125)
+    imgS = cv.resize(imgL, (0,0), None, 0.125, 0.125)
     imgS = cv.cvtColor(imgS, cv.COLOR_BGR2RGB)
 
     #encodes our video capture, with face locations found to assist with computation
-    currFaceFrame = fr.face_locations(imgS)
-    currEncodeFrame = fr.face_encodings(imgS, currFaceFrame)
+    frameLocation = fr.face_locations(imgS)
+    frameEncoding = fr.face_encodings(imgS, frameLocation)
 
     #Loop for each face found in the frame
-    for encodeFace, faceLoc in zip(currEncodeFrame, currFaceFrame):
-        #obtains the True/False state and the accuracy/face_distance for each face in the database, returned as a list
-        matches = fr.compare_faces(encodeList, encodeFace)
-        faceDis = fr.face_distance(encodeList, encodeFace)
+    for encodeFace, faceLoc in zip(frameEncoding, frameLocation):
+        #obtains the True/False state and the accuracy/face_distance for the current face, returned as a list
+        matches = fr.compare_faces(encodings, encodeFace)
+        faceDis = fr.face_distance(encodings, encodeFace)
 
-        #obtains the face index with the least face distance(highest accuracy)
-        #additional idea: if there is no match, we should change the text to say "unknown" or something
-        matchIndex = np.argmin(faceDis)
-        if matches[matchIndex]:
-            name = studentNames[matchIndex].upper()
+        if matches[0]:
             y1,x2,y2,x1 = faceLoc
             y1,x2,y2,x1 = y1*8,x2*8,y2*8,x1*8
 
             #Draws facial rectangle
-            cv.rectangle(img, (x1,y1), (x2,y2), (0,255,0), 2)
-            #Draws filled in rectangle at bottom of rectangle for text
-            cv.rectangle(img, (x1, y2-35), (x2,y2), (0,255,0), cv.FILLED)
+            cv.rectangle(imgL, (x1,y1), (x2,y2), (0,255,0), 2)
             #Draws text
-            cv.putText(img, name, (x1+6, y2-6), cv.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 2)
+            cv.putText(imgL, studentName, (x1+6, y2-6), cv.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2)
 
     
-    cv.imshow('Webcam', img)
+    cv.imshow('Webcam', imgL)
     cv.waitKey(1)
     
